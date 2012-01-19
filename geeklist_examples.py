96a1cc97f86fa3bbd039abfdd9dbc42d9ce6d177
Julien Grenier <julien.grenier42@gmail.com>
2012-01-19 00:57:29 -0500

    Fix the example;

--- geeklist_examples.py
+++ geeklist_examples.py
@@ -1,19 +1,22 @@
 # -*- coding: utf-8 -*-
 from geeklist.api import BaseGeeklistApi, GeekListOauthApi, GeekListUserApi
 
-from access import consumer_info#, access_token
+from access import consumer_info #consumer_info is { 'key': YOUR_KEY, 'secret': secret}
 
-BaseGeeklistApi.BASE_URL ='http://sandbox-api.geekli.st'
-geeklist_oauth_api = oauth_api = GeekListOauthApi(consumer_info=consumer_info)
-request_token = geeklist_oauth_api.request_token()
+BaseGeeklistApi.BASE_URL ='http://sandbox-api.geekli.st/v1'
+oauth_api = GeekListOauthApi(consumer_info=consumer_info)
+request_token = oauth_api.request_token(type='oob')
 import webbrowser
-webbrowser.open('http://sandbox.geekli.st/oauth/authorize?%s' % request_token['oauth_token'])
+webbrowser.open('http://sandbox.geekli.st/oauth/authorize?oauth_token=%s' % request_token['oauth_token'])
 
 #read verifier
-verifier = raw_input(prompt='Please enter verifier code')
-new_access_token = geeklist_oauth_api.access_token(request_token=request_token, verifier=verifier)
-BaseGeeklistApi.BASE_URL = 'http://sandbox-api.geekli.st/v1'
-user_api = GeekListUserApi(consumer_info, new_access_token)
+verifier = raw_input('Please enter verifier code>')
+oauth_access_token = oauth_api.access_token(request_token=request_token, verifier=verifier)
+access_token = {
+    'key':oauth_access_token['oauth_token'],
+    'secret':oauth_access_token['oauth_token_secret']
+}
+user_api = GeekListUserApi(consumer_info, access_token)
 print user_api.user_info()
 user_api.create_card(headline='First card created with the python wrapper API')
 
